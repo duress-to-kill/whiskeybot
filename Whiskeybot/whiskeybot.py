2015cc2d6d733dc5633345667dbb34ca4d888b6e
Cameron White <cawhite@pdx.edu>
2013-12-16 20:53:54 -0800

    Added whiskeybot module for kitnirc

---
+++ Whiskeybot/whiskeybot.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013, Cameron White
+
+from kitnirc.modular import Module
+
+class WhiskeyBot(Module):
+
+    pass
+
+module = WhiskeyBot
